74aa2b22ae18e1124c9439af7a7457f46c751047
Zerubbabel <thundervoltatbolt@gmail.com>
2024-11-10 05:06:26 +0300

    set up style & css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path")
 module.exports = {
-    mode: "development",
+    mode: 'development',
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+        ]
     }
 }
